472c7e323f4762511663f73cdf60ae41c684a5c0
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-06-05 21:22:44 +0900

    [Silver II] Title: 좌표 압축, Time: 2008 ms, Memory: 169484 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,24 +1,18 @@
 import sys
 
-def bns(arr, target, start, final):
-    mid = (start+final)//2
-
-    if arr[mid]==target:
-        return mid
-
-    if arr[mid]>target:
-        return bns(arr,target,start,mid-1)
-    elif arr[mid]<target:
-        return bns(arr,target,mid+1,final)
-
 n = int(sys.stdin.readline())
 a = list(map(int,sys.stdin.readline().split()))
 
 seta = set(a)
 aa = sorted(list(seta))
 
+dic = {}
+
+for i in range(len(aa)):
+    dic[aa[i]]=i
+
 ans = []
 for i in a:
-    ans.append(bns(aa,i,0,len(aa)-1))
+    ans.append(dic[i])
 
 print(*ans)
