0688289b8cde2bbc171982abe96830b013c3ed95
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-06-12 23:29:29 +0900

    [Silver I] Title: 미로 탐색, Time: 96 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,34 +3,30 @@ from collections import deque
 
 def sol():
     N,M = map(int,sys.stdin.readline().split())
-    grid=[]
+    grid=[[0]*(M+2)]
     for _ in range(N):
-        grid.append(list(map(int,sys.stdin.readline().rstrip())))
+        grid.append([0]+list(map(int,sys.stdin.readline().rstrip()))+[0])
+    grid.append([0]*(M+2))
 
     dq=deque()
-    dq.append((0,0))
-    
+    dq.append((1,1))
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+
     while dq:
         x,y=dq.popleft()
-        if (x,y)==(N-1,M-1):
-            print(grid[x][y])
-            break
 
-        if x-1>=0 and grid[x-1][y]==1:
-            dq.append((x-1,y))
-            grid[x-1][y]=grid[x][y]+1
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            
+            if grid[nx][ny]==0:
+                continue
 
-        if x+1<N and grid[x+1][y]==1:
-            dq.append((x+1,y))
-            grid[x+1][y]=grid[x][y]+1
+            if grid[nx][ny]==1:
+                dq.append((nx,ny))
+                grid[nx][ny]=grid[x][y] + 1      
 
-        if y-1>=0 and grid[x][y-1]==1:
-            dq.append((x,y-1))
-            grid[x][y-1]=grid[x][y]+1
-
-        if y+1<M and grid[x][y+1]==1:
-            dq.append((x,y+1))
-            grid[x][y+1]=grid[x][y]+1
-
+    print(grid[N][M])
 
 sol()
