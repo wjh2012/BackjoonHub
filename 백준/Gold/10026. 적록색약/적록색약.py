4b1de76da53ff7461c2291c3c67f12cc174d8707
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-06-19 01:43:26 +0900

    [Gold V] Title: 적록색약, Time: 284 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -3,59 +3,59 @@ import sys
 def sol():
     N=int(input())
     grid=[]
+    grid.append([0]*(N+2))
     for _ in range(N):
-        grid.append(list(sys.stdin.readline().rstrip()))
+        grid.append([0]+list(sys.stdin.readline().rstrip())+[0])
+    grid.append([0]*(N+2))
 
     mx=[1,-1,0,0]
     my=[0,0,1,-1]
 
-    visited=[]
+    grid2 = [x.copy() for x in grid]
+    
     ans = 0
-    for i in range(N):
-        for j in range(N):
-            if (i,j) not in visited:
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            if grid[i][j]:
                 dq=[]
                 dq.append((i,j))
                 while dq:
                     (x,y)=dq.pop()
-                    visited.append((x,y))
                     color = grid[x][y]
+                    grid[x][y]=0
 
                     for k in range(4):
                         nx = x+mx[k]
                         ny = y+my[k]
-                        
-                        if nx<0 or nx==N or ny<0 or ny==N:
+
+                        if grid[nx][ny]==0 or (nx,ny) in dq or grid[nx][ny]!=color:
                             continue
 
-                        if (nx,ny) not in visited and grid[nx][ny]==color:
-                            dq.append((nx,ny))
+                        dq.append((nx,ny))
                 ans+=1
 
-    visited=[]
     ans2 = 0
-    for i in range(N):
-        for j in range(N):
-            if (i,j) not in visited:
+    for i in range(N+2):
+        for j in range(N+2):
+            if grid2[i][j]:
                 dq=[]
                 dq.append((i,j))
                 while dq:
                     (x,y)=dq.pop()
-                    visited.append((x,y))
-                    color = grid[x][y]
+                    color = grid2[x][y]
+                    grid2[x][y]=0
 
                     for k in range(4):
                         nx = x+mx[k]
                         ny = y+my[k]
-                        
-                        if nx<0 or nx==N or ny<0 or ny==N:
+
+                        if grid2[nx][ny]==0 or (nx,ny) in dq:
                             continue
-                        
-                        if (nx,ny) not in visited:
-                            if color=='B' and  grid[nx][ny]==color:
-                                dq.append((nx,ny))
-                            elif color!='B' and grid[nx][ny]!='B':
-                                dq.append((nx,ny))
+                        if color=='B'and grid2[nx][ny]!='B':
+                            continue
+                        if color!='B' and grid2[nx][ny]=='B':
+                            continue
+                        dq.append((nx,ny))
                 ans2+=1
 
     print(ans,ans2)
