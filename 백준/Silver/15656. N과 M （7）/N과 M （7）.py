90bd1044ef39b817710baf6d0c0e77513b40b0c2
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-07-04 00:44:59 +0900

    [Silver III] Title: N과 M (7), Time: 2328 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+
+def sol():
+    n,m=map(int,input().split())
+    a = sorted(list(map(int,sys.stdin.readline().split())))
+    ans=[]
+
+    def dfs():
+        if len(ans)==m :
+            print(*ans)
+            return
+
+        for i in range(n):
+            ans.append(a[i])
+            dfs()
+            ans.pop()
+    dfs()
+
+sol()
