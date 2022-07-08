118d134b3e7e02252e529512ad5ec3728c8fa06d
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-07-08 11:51:21 +0900

    [Silver I] Title: 트리 순회, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,36 +1,38 @@
 import sys
-from collections import defaultdict
 
 def preorder(dfd,c):
     if c=='.':
         return
+    child = dfd[c]
     print(c,end='')
-    preorder(dfd,dfd[c][0])
-    preorder(dfd,dfd[c][1])
+    preorder(dfd,child[0])
+    preorder(dfd,child[1])
          
 def inorder(dfd,c):
     if c=='.':
         return
-    inorder(dfd,dfd[c][0])
+    child = dfd[c]
+    inorder(dfd,child[0])
     print(c,end='')
-    inorder(dfd,dfd[c][1])
+    inorder(dfd,child[1])
 
 
 def postorder(dfd,c):
     if c=='.':
         return
-    postorder(dfd,dfd[c][0])
-    postorder(dfd,dfd[c][1])
+    child = dfd[c]
+    postorder(dfd,child[0])
+    postorder(dfd,child[1])
     print(c,end='')
     
 
 def sol():
     N = int(sys.stdin.readline())
-    tree = [list(sys.stdin.readline().split()) for _ in range(N)]
-    dfd = defaultdict(list)
-    for i in tree:
-        dfd[i[0]]=i[1:]
-
+    dfd = dict()
+    for _ in range(N):
+        node, left, right = sys.stdin.readline().split()
+        dfd[node]=[left,right]
+    
     preorder(dfd,'A')
     print()
     inorder(dfd,'A')
