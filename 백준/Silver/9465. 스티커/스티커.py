2827a919f4393c9fa5eff43c5e3f87d208ed5115
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-07-10 23:29:12 +0900

    [Silver I] Title: 스티커, Time: 808 ms, Memory: 47040 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,18 @@
+import sys
+    
+def sol():
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        n = int(sys.stdin.readline())
+        sticker = [list(map(int,sys.stdin.readline().split())) for _ in range(2)]
+        
+        if n>1:
+            sticker[0][1]+=sticker[1][0]
+            sticker[1][1]+=sticker[0][0]
+            
+            for i in range(2,n):
+                sticker[0][i]+=max(sticker[1][i-1],sticker[1][i-2])
+                sticker[1][i]+=max(sticker[0][i-1],sticker[0][i-2])
+
+        print(max(sticker[0][n-1],sticker[1][n-1]))
+sol()
