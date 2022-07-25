cc9b31dce28b5e98f5ac2b17868e22a837d24e16
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-07-25 21:20:13 +0900

    [Gold IV] Title: 최단경로, Time: 712 ms, Memory: 68408 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,36 @@
+import collections
+import sys
+import heapq
+INF = 400000
+from collections import defaultdict
+
+def sol():
+    V, E = map(int,sys.stdin.readline().split())
+    K = int(sys.stdin.readline())
+    
+    G = defaultdict(list)
+    for _ in range(E):
+        u,v,w = map(int,sys.stdin.readline().split())
+        G[u].append((v,w))
+
+    K_route = [INF]*(V+1)
+    K_route[K] = 0
+
+    heap=[(K_route[K], K)]
+    while heap:
+        cur_w, node = heapq.heappop(heap)
+        if cur_w > K_route[node]:
+            continue
+        for v,w in G[node]:
+            pass_w = cur_w + w
+            if K_route[v] > pass_w:
+                K_route[v] = pass_w
+                heapq.heappush(heap, (pass_w, v))
+
+    for a in K_route[1:]:
+        if a==INF:
+            print('INF')
+        else:
+            print(a)
+
+sol()
