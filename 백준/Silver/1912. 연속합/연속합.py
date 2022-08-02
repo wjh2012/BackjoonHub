65e60931b98388338a4c8f09e9737c71c934077b
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-08-03 01:23:33 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 38548 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,21 +2,14 @@ import sys
 
 def sol():
     n = int(input())
-    num = [0]+list(map(int,sys.stdin.readline().split()))
+    num = list(map(int,sys.stdin.readline().split()))
 
-    dp = [0]*(n+1)
-    dp[0] = -1000
+    dp = [0]*n
+    dp[0] = num[0]
 
-    ans = -1000
-    for i in range(1,n+1):
-        if dp[i-1] < 0 and num[i] < 0:
-            dp[i] = max(num[i], dp[i-1])
-        else:
-            k = dp[i-1] + num[i]
-            dp[i] = max(k, num[i])
+    for i in range(1,n):
+        dp[i] = max(num[i], dp[i-1]+num[i])
 
-        ans = max(ans, dp[i])
-
-    print(ans)
+    print(max(dp))
 
 sol()
