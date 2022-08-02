9adb64713debac8ab8da48645b4f4681100ba947
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-08-03 01:19:36 +0900

    [Silver II] Title: 연속합, Time: 144 ms, Memory: 38548 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,22 @@
+import sys
+
+def sol():
+    n = int(input())
+    num = [0]+list(map(int,sys.stdin.readline().split()))
+
+    dp = [0]*(n+1)
+    dp[0] = -1000
+
+    ans = -1000
+    for i in range(1,n+1):
+        if dp[i-1] < 0 and num[i] < 0:
+            dp[i] = max(num[i], dp[i-1])
+        else:
+            k = dp[i-1] + num[i]
+            dp[i] = max(k, num[i])
+
+        ans = max(ans, dp[i], num[i])
+
+    print(ans)
+
+sol()
