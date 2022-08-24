ef6f549fdd0b0465061d42c9d27ea27b5db68048
GGOMG <57653698+wjh2012@users.noreply.github.com>
2022-08-24 20:37:45 +0900

    [Gold V] Title: 내려가기, Time: 268 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -2,13 +2,10 @@ import sys
 
 def sol():
     N = int(sys.stdin.readline())
-    a,b,c = 0,0,0
-    x,y,z = 0,0,0
-    for i in range(N):
+    a,b,c =  map(int,sys.stdin.readline().split())
+    x,y,z = a,b,c
+    for _ in range(1,N):
         A,B,C = map(int,sys.stdin.readline().split())
-        if i==0:
-            x,y,z = a,b,c = A,B,C
-            continue
         a,b,c = A + max(a,b), B + max(a,b,c), C + max(b,c)
         x,y,z = A + min(x,y), B + min(x,y,z), C + min(y,z)
 
